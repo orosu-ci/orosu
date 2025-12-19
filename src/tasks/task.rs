27f94a7a6df699bb80031c2adfcdc6bd44aafca7
Ilya Nixan <thenixan@gmail.com>
2025-12-19 22:23:07 +0300

    Removed unsafe code

--- src/tasks/task.rs
+++ src/tasks/task.rs
@@ -67,22 +67,10 @@ impl Task {
         #[cfg(target_os = "linux")]
         {
             if let Some(username) = script.run_as {
-                unsafe {
-                    command.pre_exec(move || {
-                        let user = users::get_user_by_name(&username).ok_or_else(|| {
-                            std::io::Error::new(std::io::ErrorKind::NotFound, "User not found")
-                        })?;
-
-                        nix::unistd::setgid(nix::unistd::Gid::from_raw(user.primary_group_id()))
-                            .map_err(|e| {
-                                std::io::Error::new(std::io::ErrorKind::PermissionDenied, e)
-                            })?;
-                        nix::unistd::setuid(nix::unistd::Uid::from_raw(user.uid())).map_err(
-                            |e| std::io::Error::new(std::io::ErrorKind::PermissionDenied, e),
-                        )?;
-                        Ok(())
-                    });
-                }
+                let user = users::get_user_by_name(&username).ok_or_else(|| {
+                    std::io::Error::new(std::io::ErrorKind::NotFound, "User not found")
+                })?;
+                command.uid(user.uid());
             };
         }
 
